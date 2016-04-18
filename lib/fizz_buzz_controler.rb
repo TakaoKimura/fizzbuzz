4d328b40339cbd9b1dbea9ed606a815fa01ab139
木村卓央 <takao@kimuratakuou-no-MacBook-Pro.local>
2016-04-19 08:36:24 +0900

    Memory追加

--- lib/fizz_buzz_controler.rb
+++ lib/fizz_buzz_controler.rb
@@ -1,2 +1,15 @@
+require 'fizz_buzz'
+
 class FizzBuzzControler
+
+  def initialize
+    @temporary = Array.new
+  end
+
+  def fizzbuzz(value)
+    @temporary.push(FizzBuzz.call(value))
+  end
+  def memory
+    @temporary
+  end
 end
--- spec/fizz_buzz_controler_spec.rb
+++ spec/fizz_buzz_controler_spec.rb
@@ -1,5 +1,16 @@
 require 'fizz_buzz_controler'
 
 describe FizzBuzzControler do
-  
+  describe '#memory' do
+    it 'fizzbuzzが実施された記録が残っていること' do
+      controler = FizzBuzzControler.new
+      controler.fizzbuzz(3)
+      controler.fizzbuzz(5)
+      controler.fizzbuzz(15)
+      controler.fizzbuzz(1)
+      controler.fizzbuzz(0)
+      expect(controler.memory).to eq ['Fizz','Buzz','FizzBuzz','1','0']
+    end
+  end
+
 end
